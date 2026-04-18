5a955b0c8463a4b3629f75a622a2f599f3469d98
karo <karolin.kostial@gmail.com>
2026-04-18 19:48:42 +0200

    test: add e2e tests for KeylineApplication controller

--- internal/controller/keylineapplication_controller_test.go
+++ internal/controller/keylineapplication_controller_test.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
@@ -21,64 +5,230 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	keylinev1alpha1 "github.com/keyline/keyline-operator/api/v1alpha1"
 )
 
 var _ = Describe("KeylineApplication Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+	const (
+		appName   = "test-app"
+		projName  = "test-app-proj"
+		vsName    = "test-app-vs"
+		instName  = "test-app-instance"
+		namespace = "default"
+	)
 
-		ctx := context.Background()
+	ctx := context.Background()
+	appNamespacedName := types.NamespacedName{Name: appName, Namespace: namespace}
+	projNamespacedName := types.NamespacedName{Name: projName, Namespace: namespace}
+	vsNamespacedName := types.NamespacedName{Name: vsName, Namespace: namespace}
+	instNamespacedName := types.NamespacedName{Name: instName, Namespace: namespace}
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+	newReconciler := func() *KeylineApplicationReconciler {
+		return &KeylineApplicationReconciler{
+			Client: k8sClient,
+			Scheme: k8sClient.Scheme(),
 		}
-		keylineapplication := &keylinev1alpha1.KeylineApplication{}
+	}
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind KeylineApplication")
-			err := k8sClient.Get(ctx, typeNamespacedName, keylineapplication)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &keylinev1alpha1.KeylineApplication{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
+	AfterEach(func() {
+		cleanup := []struct {
+			nn  types.NamespacedName
+			obj k8sclient.Object
+		}{
+			{appNamespacedName, &keylinev1alpha1.KeylineApplication{}},
+			{projNamespacedName, &keylinev1alpha1.KeylineProject{}},
+			{vsNamespacedName, &keylinev1alpha1.KeylineVirtualServer{}},
+			{instNamespacedName, &keylinev1alpha1.KeylineInstance{}},
+		}
+		for _, c := range cleanup {
+			if err := k8sClient.Get(ctx, c.nn, c.obj); err == nil {
+				Expect(k8sClient.Delete(ctx, c.obj)).To(Succeed())
+			}
+		}
+	})
+
+	newApplication := func(projRef string) *keylinev1alpha1.KeylineApplication {
+		return &keylinev1alpha1.KeylineApplication{
+			ObjectMeta: metav1.ObjectMeta{Name: appName, Namespace: namespace},
+			Spec: keylinev1alpha1.KeylineApplicationSpec{
+				ProjectRef:   corev1.LocalObjectReference{Name: projRef},
+				Name:         "test-application",
+				DisplayName:  "Test Application",
+				Type:         "public",
+				RedirectUris: []string{"http://localhost:8080/callback"},
+			},
+		}
+	}
+
+	newProject := func(vsRef string) *keylinev1alpha1.KeylineProject {
+		return &keylinev1alpha1.KeylineProject{
+			ObjectMeta: metav1.ObjectMeta{Name: projName, Namespace: namespace},
+			Spec: keylinev1alpha1.KeylineProjectSpec{
+				VirtualServerRef: corev1.LocalObjectReference{Name: vsRef},
+				Slug:             "test-proj",
+				Name:             "Test Project",
+			},
+		}
+	}
+
+	newVirtualServer := func(instanceRef string) *keylinev1alpha1.KeylineVirtualServer {
+		return &keylinev1alpha1.KeylineVirtualServer{
+			ObjectMeta: metav1.ObjectMeta{Name: vsName, Namespace: namespace},
+			Spec: keylinev1alpha1.KeylineVirtualServerSpec{
+				InstanceRef: corev1.LocalObjectReference{Name: instanceRef},
+				Name:        "testvs",
+			},
+		}
+	}
+
+	newInstance := func() *keylinev1alpha1.KeylineInstance {
+		return &keylinev1alpha1.KeylineInstance{
+			ObjectMeta: metav1.ObjectMeta{Name: instName, Namespace: namespace},
+			Spec: keylinev1alpha1.KeylineInstanceSpec{
+				Image:               "ghcr.io/the127/keyline:latest",
+				ExternalUrl:         "http://keyline.example.com",
+				FrontendExternalUrl: "http://app.example.com",
+				Database: keylinev1alpha1.KeylineInstanceDatabaseSpec{
+					Mode: "postgres",
+					Postgres: &keylinev1alpha1.KeylineInstancePostgresSpec{
+						Host:                 "postgres",
+						CredentialsSecretRef: corev1.LocalObjectReference{Name: "pg-creds"},
 					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
+				},
+				KeyStore: keylinev1alpha1.KeylineInstanceKeyStoreSpec{Mode: "directory"},
+			},
+		}
+	}
+
+	readyCondition := func() metav1.Condition {
+		return metav1.Condition{
+			Type:               keylinev1alpha1.ConditionReady,
+			Status:             metav1.ConditionTrue,
+			Reason:             "Synced",
+			LastTransitionTime: metav1.Now(),
+		}
+	}
+
+	assertNotReady := func(reason string) {
+		app := &keylinev1alpha1.KeylineApplication{}
+		Expect(k8sClient.Get(ctx, appNamespacedName, app)).To(Succeed())
+		cond := meta.FindStatusCondition(app.Status.Conditions, keylinev1alpha1.ConditionReady)
+		Expect(cond).NotTo(BeNil())
+		Expect(cond.Status).To(Equal(metav1.ConditionFalse))
+		Expect(cond.Reason).To(Equal(reason))
+	}
+
+	Context("when the referenced KeylineProject does not exist", func() {
+		BeforeEach(func() {
+			Expect(k8sClient.Create(ctx, newApplication("nonexistent-proj"))).To(Succeed())
 		})
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &keylinev1alpha1.KeylineApplication{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+		It("sets Ready=False with reason ProjectNotFound", func() {
+			_, err := newReconciler().Reconcile(ctx, reconcile.Request{NamespacedName: appNamespacedName})
 			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance KeylineApplication")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+			assertNotReady("ProjectNotFound")
 		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &KeylineApplicationReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
+	})
 
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
+	Context("when the referenced KeylineProject is not Ready", func() {
+		BeforeEach(func() {
+			Expect(k8sClient.Create(ctx, newProject(vsName))).To(Succeed())
+			Expect(k8sClient.Create(ctx, newApplication(projName))).To(Succeed())
+		})
+
+		It("sets Ready=False with reason ProjectNotReady", func() {
+			_, err := newReconciler().Reconcile(ctx, reconcile.Request{NamespacedName: appNamespacedName})
 			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+			assertNotReady("ProjectNotReady")
+		})
+	})
+
+	Context("when the referenced KeylineVirtualServer does not exist", func() {
+		BeforeEach(func() {
+			proj := newProject("nonexistent-vs")
+			Expect(k8sClient.Create(ctx, proj)).To(Succeed())
+			proj.Status.Conditions = []metav1.Condition{readyCondition()}
+			Expect(k8sClient.Status().Update(ctx, proj)).To(Succeed())
+
+			Expect(k8sClient.Create(ctx, newApplication(projName))).To(Succeed())
+		})
+
+		It("sets Ready=False with reason VirtualServerNotFound", func() {
+			_, err := newReconciler().Reconcile(ctx, reconcile.Request{NamespacedName: appNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+			assertNotReady("VirtualServerNotFound")
+		})
+	})
+
+	Context("when the referenced KeylineVirtualServer is not Ready", func() {
+		BeforeEach(func() {
+			Expect(k8sClient.Create(ctx, newVirtualServer(instName))).To(Succeed())
+
+			proj := newProject(vsName)
+			Expect(k8sClient.Create(ctx, proj)).To(Succeed())
+			proj.Status.Conditions = []metav1.Condition{readyCondition()}
+			Expect(k8sClient.Status().Update(ctx, proj)).To(Succeed())
+
+			Expect(k8sClient.Create(ctx, newApplication(projName))).To(Succeed())
+		})
+
+		It("sets Ready=False with reason VirtualServerNotReady", func() {
+			_, err := newReconciler().Reconcile(ctx, reconcile.Request{NamespacedName: appNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+			assertNotReady("VirtualServerNotReady")
+		})
+	})
+
+	Context("when the referenced KeylineInstance does not exist", func() {
+		BeforeEach(func() {
+			vs := newVirtualServer("nonexistent-instance")
+			Expect(k8sClient.Create(ctx, vs)).To(Succeed())
+			vs.Status.Conditions = []metav1.Condition{readyCondition()}
+			Expect(k8sClient.Status().Update(ctx, vs)).To(Succeed())
+
+			proj := newProject(vsName)
+			Expect(k8sClient.Create(ctx, proj)).To(Succeed())
+			proj.Status.Conditions = []metav1.Condition{readyCondition()}
+			Expect(k8sClient.Status().Update(ctx, proj)).To(Succeed())
+
+			Expect(k8sClient.Create(ctx, newApplication(projName))).To(Succeed())
+		})
+
+		It("sets Ready=False with reason InstanceNotFound", func() {
+			_, err := newReconciler().Reconcile(ctx, reconcile.Request{NamespacedName: appNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+			assertNotReady("InstanceNotFound")
+		})
+	})
+
+	Context("when the operator credentials Secret is missing", func() {
+		BeforeEach(func() {
+			Expect(k8sClient.Create(ctx, newInstance())).To(Succeed())
+
+			vs := newVirtualServer(instName)
+			Expect(k8sClient.Create(ctx, vs)).To(Succeed())
+			vs.Status.Conditions = []metav1.Condition{readyCondition()}
+			Expect(k8sClient.Status().Update(ctx, vs)).To(Succeed())
+
+			proj := newProject(vsName)
+			Expect(k8sClient.Create(ctx, proj)).To(Succeed())
+			proj.Status.Conditions = []metav1.Condition{readyCondition()}
+			Expect(k8sClient.Status().Update(ctx, proj)).To(Succeed())
+
+			Expect(k8sClient.Create(ctx, newApplication(projName))).To(Succeed())
+		})
+
+		It("sets Ready=False with reason SecretNotFound", func() {
+			_, err := newReconciler().Reconcile(ctx, reconcile.Request{NamespacedName: appNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+			assertNotReady("SecretNotFound")
 		})
 	})
 })
